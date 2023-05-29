387e2c07c601c263a6e85922eb6b8c1215c82acc
Rojajj <98951638+Rojajj@users.noreply.github.com>
2023-05-29 15:45:42 +0530

    pullrequest_B2_master
    Changes

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -5,22 +5,22 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<title>LiteChat</title>
 		<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
-		<link rel="stylesheet" href="{!! asset('public/assets/css/all.min.css') !!}">
+		<link rel="stylesheet" href="{!! asset('assets/css/all.min.css') !!}">
 		<link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
-		<link rel="stylesheet" href="{!! asset('public/assets/css/tempusdominus-bootstrap-4.min.css') !!}">
-		<link rel="stylesheet" href="{!! asset('public/assets/css/icheck-bootstrap.min.css') !!}">
-		<link rel="stylesheet" href="{!! asset('public/assets/css/adminlte.min.css') !!}">
-		<link rel="stylesheet" href="{!! asset('public/assets/css/OverlayScrollbars.min.css') !!}">
+		<link rel="stylesheet" href="{!! asset('assets/css/tempusdominus-bootstrap-4.min.css') !!}">
+		<link rel="stylesheet" href="{!! asset('assets/css/icheck-bootstrap.min.css') !!}">
+		<link rel="stylesheet" href="{!! asset('assets/css/adminlte.min.css') !!}">
+		<link rel="stylesheet" href="{!! asset('assets/css/OverlayScrollbars.min.css') !!}">
 		<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.3.0/css/datepicker.min.css">
-		<link rel="stylesheet" href="{!! asset('public/assets/css/dataTables.bootstrap4.min.css') !!}">
-		<link rel="stylesheet" href="{!! asset('public/assets/css/responsive.bootstrap4.min.css') !!}">
-		<link rel="stylesheet" href="{!! asset('public/assets/css/buttons.bootstrap4.min.css') !!}">
+		<link rel="stylesheet" href="{!! asset('assets/css/dataTables.bootstrap4.min.css') !!}">
+		<link rel="stylesheet" href="{!! asset('assets/css/responsive.bootstrap4.min.css') !!}">
+		<link rel="stylesheet" href="{!! asset('assets/css/buttons.bootstrap4.min.css') !!}">
 	</head>
 	<body class="hold-transition sidebar-mini layout-fixed">
 		<div class="wrapper">
 
 			<div class="preloader flex-column justify-content-center align-items-center">
-				<img class="animation__shake" src="{!! asset('public/assets/images/AdminLTELogo.png" alt="AdminLTELogo') !!}" height="60" width="60">
+				<img class="animation__shake" src="{!! asset('assets/images/AdminLTELogo.png" alt="AdminLTELogo') !!}" height="60" width="60">
 			</div>
 
 			@include('layouts.nav')
@@ -36,21 +36,21 @@
 			</aside>
 		</div>
 
-		<script src="{!! asset('public/assets/js/jquery.min.js') !!}"></script>
-		<script src="{!! asset('public/assets/js/jquery-ui.min.js') !!}"></script>
+		<script src="{!! asset('assets/js/jquery.min.js') !!}"></script>
+		<script src="{!! asset('assets/js/jquery-ui.min.js') !!}"></script>
 		<script>
 			$.widget.bridge('uibutton', $.ui.button)
 		</script>
-		<script src="{!! asset('public/assets/js/bootstrap.bundle.min.js') !!}"></script>
-		<script src="{!! asset('public/assets/js/sparkline.js') !!}"></script>
-		<script src="{!! asset('public/assets/js/jquery.knob.min.js') !!}"></script>
-		<script src="{!! asset('public/assets/js/tempusdominus-bootstrap-4.min.js') !!}"></script>
-		<script src="{!! asset('public/assets/js/jquery.overlayScrollbars.min.js') !!}"></script>
-		<script src="{!! asset('public/assets/js/adminlte.min.js') !!}"></script>
-		<script src="{!! asset('public/assets/js/demo.js') !!}"></script>
-		<script src="{!! asset('public/assets/js/dashboard.js') !!}"></script>
-		<script type="text/javascript" src="{!! asset('public/assets/js/jquery.validate.min.js') !!}"></script>
-		<script type="text/javascript" src="{!! asset('public/assets/js/additional-methods.min.js') !!}"></script>
+		<script src="{!! asset('assets/js/bootstrap.bundle.min.js') !!}"></script>
+		<script src="{!! asset('assets/js/sparkline.js') !!}"></script>
+		<script src="{!! asset('assets/js/jquery.knob.min.js') !!}"></script>
+		<script src="{!! asset('assets/js/tempusdominus-bootstrap-4.min.js') !!}"></script>
+		<script src="{!! asset('assets/js/jquery.overlayScrollbars.min.js') !!}"></script>
+		<script src="{!! asset('assets/js/adminlte.min.js') !!}"></script>
+		<script src="{!! asset('assets/js/demo.js') !!}"></script>
+		<script src="{!! asset('assets/js/dashboard.js') !!}"></script>
+		<script type="text/javascript" src="{!! asset('assets/js/jquery.validate.min.js') !!}"></script>
+		<script type="text/javascript" src="{!! asset('assets/js/additional-methods.min.js') !!}"></script>
 		<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.3.0/js/bootstrap-datepicker.min.js"></script>
 		@yield('script')
 	</body>
--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -1,13 +1,13 @@
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
 	<a href="javascript:void(0);" class="brand-link">
-		<img src="{!! asset('public/assets/images/AdminLTELogo.png') !!}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+		<img src="{!! asset('assets/images/AdminLTELogo.png') !!}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
 		<span class="brand-text font-weight-light">LiteChat</span>
 	</a>
 
 	<div class="sidebar">
 		<div class="user-panel mt-3 pb-3 mb-3 d-flex">
 			<div class="image">
-				<img src="{!! asset('public/assets/images/user2-160x160.jpg') !!}" class="img-circle elevation-2" alt="User Image">
+				<img src="{!! asset('assets/images/user2-160x160.jpg') !!}" class="img-circle elevation-2" alt="User Image">
 			</div>
 			<div class="info">
 				<?php $user_data = Session::get('user_data');

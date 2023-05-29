f3f0c7d4997e4a6ea86ca8b6504537d21d16a679
Rojajj <98951638+Rojajj@users.noreply.github.com>
2023-05-29 15:35:18 +0530

    pullrequest_B2_master
    Changes

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -5,7 +5,7 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<title>LiteChat</title>
 		<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
-		<link rel="stylesheet" href="{!! asset('public/assets/css/all.min.css') !!}">
+		<link rel="stylesheet" href="{!! asset('assets/css/all.min.css') !!}">
 		<link rel="stylesheet" href="{!! asset('public/assets/css/icheck-bootstrap.min.css') !!}">
 		<link rel="stylesheet" href="{!! asset('public/assets/css/adminlte.min.css') !!}">
 	</head>

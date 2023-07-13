811132fd09670c3228e72b3757ee76b9bfa69032
Rojajj <98951638+Rojajj@users.noreply.github.com>
2023-07-13 17:13:13 +0530

    pullrequest_B19_master
    changes

--- app/Http/Controllers/APICommunicationController.php
+++ app/Http/Controllers/APICommunicationController.php
@@ -522,7 +522,7 @@ class APICommunicationController extends Controller
             // $read_count = CommunicationRecipients::select(DB::raw('count(*) as count'),'communication_id')->where(['message_status'=>Config::get('app.Read')])->groupBy('communication_id')->get()->toArray(); //get read count based on notification id.
             // $readcount_data = array_column($read_count,'count','communication_id');
             $notification_ids = CommunicationRecipients::where(function($query) use ($communication_id_list,$userdetails,$user) {
-                    $query->where(['user_table_id'=>$userdetails->id,'user_role'=>$user->user_role,'communication_type'=>1])->whereIn('communication_id',$communication_id_list);
+                    $query->where(['user_tablqe_id'=>$userdetails->id,'user_role'=>$user->user_role,'communication_type'=>1])->whereIn('communication_id',$communication_id_list);
                 })->orwhere(function($query) use ($userdetails,$user,$newsevents_id_list) {
                     $query->where(['user_table_id'=>$userdetails->id,'user_role'=>$user->user_role,'communication_type'=>2])->whereIn('communication_id',$newsevents_id_list);
                 })->orderBy('actioned_time')->get()->toArray(); //Fetch applicable notification ids from table for logged in user.

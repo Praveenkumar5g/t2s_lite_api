25380a86239c80643c96e192b8f19bff807907bd
Rojajj <98951638+Rojajj@users.noreply.github.com>
2023-10-03 16:55:15 +0530

    changes

--- app/Http/Controllers/APICommunicationController.php
+++ app/Http/Controllers/APICommunicationController.php
@@ -1171,9 +1171,8 @@ class APICommunicationController extends Controller
         }
         else if($userdetails['user_role'] == Config::get('app.Parent_role'))
         {
-            echo 'tet';exit;
             $user_category = 'Parent';
-            $user_details = UserParents::where(['id'=>$userdetails['user_table_id']])->get()->first();//fetch id from user all table to store notification triggered user
+            $user_details = UserParents::where(['isd'=>$userdetails['user_table_id']])->get()->first();//fetch id from user all table to store notification triggered user
             echo '<pre>';print_r($user_details);exit;
         }
         return (['user_details'=>$user_details,'user_category'=>$user_category]);

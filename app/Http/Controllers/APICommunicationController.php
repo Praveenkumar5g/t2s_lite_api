26fd7ea26f7826b3f1ed929b578b64ffbc1d04cf
Rojajj <98951638+Rojajj@users.noreply.github.com>
2023-10-04 16:23:25 +0530

    changes

--- app/Http/Controllers/APICommunicationController.php
+++ app/Http/Controllers/APICommunicationController.php
@@ -1553,13 +1553,13 @@ class APICommunicationController extends Controller
         $player_details = array_column(Appusers::get()->toArray(),'player_id','loginid');
         $group_users = UserGroupsMapping::where('group_id',$request->group_id)->where('user_status',Config::get('app.Group_Active')); //fetch group members
 
-        if(isset($request->search) && $request->search!='')
-        {
-            // $staff_list = $staff_list->where('first_name', 'like', '%' . $request->search . '%')->orWhere('mobile_number', 'like', '%' . $request->search . '%');
-        }
-        else
+        // if(isset($request->search) && $request->search!='')
+        // {
+        //     // $staff_list = $staff_list->where('first_name', 'like', '%' . $request->search . '%')->orWhere('mobile_number', 'like', '%' . $request->search . '%');
+        // }
+        // else
             $group_users =$group_users->groupBy('user_role')->get();
-
+        echo '<pre>';print_r($group_users);exit;
         // $currentPage = LengthAwarePaginator::resolveCurrentPage(); // Get current page form url e.x. &page=1
         $currentPage = $request->page;
         $itemCollection = new Collection($group_users); // Create a new Laravel collection from the array data

3101a838aab72924e091dbfd4fa5120f6369b1df
Rojajj <98951638+Rojajj@users.noreply.github.com>
2023-11-15 12:03:52 +0530

    pullrequest_B44_master
    changes

--- app/Http/Controllers/Version2/V2APINewsEventsController.php
+++ app/Http/Controllers/Version2/V2APINewsEventsController.php
@@ -246,7 +246,7 @@ class V2APINewsEventsController extends Controller
         $images['prev_page_url'] = $tempdata['prev_page_url'];
         $images['from'] = $tempdata['from'];
         $images['to'] = $tempdata['to'];
-            echo '<pre>';print_r($tempdata);
+            echo '<pre>';print_r($tempdata);print_r($itemCollection);print_r($newsevents);
             exit;
 
         foreach ($tempdata['data'] as $key => $value) { //loop to format all the data in display formaat

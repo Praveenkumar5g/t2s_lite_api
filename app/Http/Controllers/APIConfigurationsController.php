09b621ccb7124f87a728d70566b0050e3fea4edd
Rojajj <98951638+Rojajj@users.noreply.github.com>
2024-01-29 13:51:33 +0530

    pullrequest_B46_master
    changes

--- app/Http/Controllers/APIConfigurationsController.php
+++ app/Http/Controllers/APIConfigurationsController.php
@@ -2329,8 +2329,10 @@ class APIConfigurationsController extends Controller
             // ]
         ]);
 
+        $profile = SchoolDatabase::where(['school_id'=>$data['school_profile_id']])->get()->first();
+
         // return token 
-        return response()->json(compact('configuration'));
+        return response()->json(compact('configuration','profile'));
 	}
 
     /*----------------------------Onboarding Manual--------------------------------*/    

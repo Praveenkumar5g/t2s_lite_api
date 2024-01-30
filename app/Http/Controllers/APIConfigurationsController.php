1a0063c63368be71e5fd620009c7638fe6606535
Rojajj <98951638+Rojajj@users.noreply.github.com>
2024-01-30 13:40:44 +0530

    pullrequest_B46_master
    changes

--- app/Http/Controllers/APIConfigurationsController.php
+++ app/Http/Controllers/APIConfigurationsController.php
@@ -2331,11 +2331,7 @@ class APIConfigurationsController extends Controller
         ]);
 
         $profile = SchoolDatabase::where(['school_id'=>$userdata->school_profile_id])->get()->first();
-       	if($_SERVER['SERVER_NAME'] == 'qaliteapi.timetoschool.com')
-       		$excel =(file_exists(public_path('/public/uploads/Onboarding.xlsx'))?Config::get('app.QA_URL').'public/uploads/Onboarding.xlsx':'');
-       	else
-       		$excel =(file_exists(public_path('/public/uploads/Onboarding.xlsx'))?Config::get('app.LIVE_URL').'public/uploads/Onboarding.xlsx':'');
-
+       	$excel =(file_exists(public_path('/uploads/Onboarding.xlsx'))?Config::get('app.QA_URL').'uploads/Onboarding.xlsx':'');
         // return token 
         return response()->json(['configuration'=>$configuration,'profile'=>$profile,'excel'=>$excel]);
 	}

98307a66e0490bb40de0f2dd0c4a41ea82e596e7
Rojajj <98951638+Rojajj@users.noreply.github.com>
2024-02-09 10:59:40 +0530

    pullrequest_B46_master
    changes

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -42,7 +42,7 @@ class WelcomeController extends Controller
             // $file->move(public_path().'/uploads/'.$school_code, $names);
 
             $data = base64_decode($file);
-            $name = $file_name.''.(time()+$index).'.'.$ext[$index];
+            $name = $file_name[$index].''.(time()+$index).'.'.$ext[$index];
             $file = public_path().'/'.env('SAMPLE_CONFIG_URL').$school_code.$target_file.$name;
             file_put_contents($file, $data);
 
@@ -82,7 +82,7 @@ class WelcomeController extends Controller
             // $file->move(public_path().'/uploads/'.$school_code, $names);  
 
             $data = base64_decode($file);
-            $name = $file_name.''.(time()+$index).'.'.$ext[$index];
+            $name = $file_name[$index].''.(time()+$index).'.'.$ext[$index];
             $file = public_path().'/'.env('SAMPLE_CONFIG_URL').$school_code.$target_file.$name;
             file_put_contents($file, $data);
 
